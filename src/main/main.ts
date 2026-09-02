8aa0c09fc8c7bd1f37cedf0d42342e6c2ea45d9a
Materia597 <zelosdiaperbutt@gmail.com>
2026-09-01 18:42:39 -0700

    Made the window slightly bigger.

--- src/main/main.ts
+++ src/main/main.ts
@@ -13,8 +13,8 @@ const initialPage = (hasBeenSetup: boolean): string => {
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1000,
+        height: 700,
         webPreferences: {
             preload: path.join(__dirname, '../preload/preload.js')
         }
