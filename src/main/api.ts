4e381fa52523cc418e0911d3870b77ea71e1392c
Materia597 <zelosdiaperbutt@gmail.com>
2026-09-09 23:02:14 -0700

    Made that an export.
    The API is probably going to be split up across the different actions with only something like the getHeaders function being in that file still.

--- src/main/api.ts
+++ src/main/api.ts
@@ -3,7 +3,7 @@ import { getAPIKey, getUsername } from './workers/saveSecure'
 import { readFile } from "node:fs/promises"
 
 
-const getHeaders = () => {
+export const getHeaders = () => {
     return {
         "Authorization": "Basic " + btoa(`${getUsername()}:${getAPIKey()}`),
         "User-Agent": "E621EasyExperience/1.0 (by zelosdiaperbutt on e621)"
